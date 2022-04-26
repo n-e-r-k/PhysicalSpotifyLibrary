187348d0e5a9b028cbf441703727aeca33eb764b
Jacob France <jnf019@latech.edu>
2022-04-25 22:17:19 -0500

    PSL Update

--- Code/PSL/PSL.py
+++ Code/PSL/PSL.py
@@ -5,4 +5,17 @@ class PSL():
     def __init__(self, spotifyUsername, spotipyClientID, spotipyClientSecret, spotifyDevice, libraryDirectory = '/data.pkl', redirectURL = 'https://www.google.com/', scope = 'user-read-private user-read-playback-state user-modify-playback-state'):
         pass
     
-    def K
+    def load(self):
+        pass
+
+    def save(self):
+        pass
+
+    def connect(self):
+        pass
+
+    def read(self):
+        pass
+
+    def play(self, UID):
+        pass
