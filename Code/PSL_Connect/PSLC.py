62a5d8d621036d2d7b6bc6dbab84614aeef33b6f
Brayden Fisher <braydenfisher02@gmail.com>
2022-04-29 01:40:34 -0500

    Updates to PSLC

--- Code/PSL_Connect/PSLC.py
+++ Code/PSL_Connect/PSLC.py
@@ -11,6 +11,12 @@ import spotipy
 import webbrowser
 import spotipy.util as util
 from json.decoder import JSONDecodeError
+
+username = 'Fish'
+SPOTIPY_CLIENT_ID = 'c2f887c733274f7087836d78574fb588'
+SPOTIPY_CLIENT_SECRET='e7be691ec4254d839d0552a085736089'
+SPOTIPY_REDIRECT_URI='https://www.google.com/'
+scope = 'user-read-private user-read-playback-state user-modify-playback-state'
 class PSL_C(Canvas): 
     
     # Get track information
--- Code/PSL_Connect/PSLC_Test.py
+++ Code/PSL_Connect/PSLC_Test.py
@@ -1,19 +1,56 @@
-import spotipy
-import spotipy.util as util
+import tkinter as tk #Tkinter import for GUI
+from PIL import ImageTk,Image #PIL import for image manipulation
+from time import sleep
+import urllib.request # Needed for API access and image download
+import io # Needed to manipulate image in memory
+import asyncio # async
 
-username = ''
-SPOTIPY_CLIENT_ID = ''
-SPOTIPY_CLIENT_SECRET=''
-SPOTIPY_REDIRECT_URI='https://www.google.com/'
-scope = 'user-read-private user-read-playback-state user-modify-playback-state'
+#Setups Tkinter GUI
+def tkinter_init():
+    root = tk.Tk() # root is tkinter obj
+    root.geometry(f"{root.winfo_screenheight()}x{root.winfo_screenwidth()}") # sets window size to max screen size
+    #root.geometry("500x500")
+    root.attributes('-fullscreen', True) # sets to fullscreen mode
+    return root # return tk obj
 
-lz_uri = 'spotify:artist:36QJpDe2go2KgaRleHCDTp'
+def close(event):
+    quit()
 
-spotify = spotipy.Spotify(util.prompt_for_user_token(username, scope, SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET, SPOTIPY_REDIRECT_URI))
-results = spotify.artist_top_tracks(lz_uri)
+def spotify_setup():
+    return None
 
-for track in results['tracks'][:10]:
-    print('track    : ' + track['name'])
-    print('audio    : ' + track['preview_url'])
-    print('cover art: ' + track['album']['images'][0]['url'])
-    print()
+def resize_image(img, root):
+    min_size = min(root.winfo_screenheight(), root.winfo_screenwidth())
+    return img.resize((min_size, min_size))
+
+def center_image(img, root):
+    width = (root.winfo_screenwidth() - img.width) // 2
+    height = (root.winfo_screenheight() - img.height) // 2
+    return width, height
+
+def download_image(image_url):
+    with urllib.request.urlopen(image_url) as url:
+        f = io.BytesIO(url.read())
+    return Image.open(f)
+
+def display_image(root):
+    canvas = tk.Canvas(root)
+    canvas.config(background="black") # change background to black
+    canvas.pack(expand=True, fill='both') # make canvas entire tk
+    img = download_image("https://i.scdn.co/image/ab67616d0000b273c03f8ef4160b2a3b72dd407d") # get image
+    img = resize_image(img, root) if img.height != root.winfo_screenheight() and img.width != root.winfo_screenwidth() else img
+    width, height = center_image(img, root)
+    img = ImageTk.PhotoImage(img)
+    canvas.create_image(width, height, image=img, anchor='nw')
+    root.bind('<Escape>', close)
+    root.mainloop()
+
+
+def main():
+    root = tkinter_init()
+    #print(f"{root.winfo_screenheight()}x{root.winfo_screenwidth()}")
+    display_image(root)
+    
+
+if __name__ == "__main__":
+    main()
