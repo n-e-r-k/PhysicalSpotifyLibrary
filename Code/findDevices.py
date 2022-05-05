302b3fd22fbb4e371d39856c1a3bef63251b1d9e
Jacob France <jnf019@latech.edu>
2022-05-05 03:52:49 -0500

    findDevices Update

--- Code/findDevices.py
+++ Code/findDevices.py
@@ -4,6 +4,8 @@ main = PSL('/home/pi/credentials.csv', debugStatus = 3)
 
 devNum = 0
 
-for device in main.spotifyObject.devices:
+devices = main.spotifyObject.devices
+
+for device in devices:
     print(f"device {devNum}:" + device)
     devNum += 1
