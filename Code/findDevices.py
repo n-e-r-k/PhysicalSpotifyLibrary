d2db8e81937c20c035ea6ef6040ada19a5d22b7c
Jacob France <Jacob_France@outlook.com>
2022-05-05 00:42:58 -0500

    findDevices Update

--- Code/findDevices.py
+++ Code/findDevices.py
@@ -2,4 +2,4 @@ from PSL import PSL
 
 main = PSL('/home/pi/credentials.csv', debugStatus = 3)
 
-print("Available Devices:" + PSL.spotifyObject.devices())
+print("Available Devices:" + main.spotifyObject.devices())
